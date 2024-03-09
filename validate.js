8ed0001a20fe803d8f9417a410c30d669f711945
ChristelPeerisR <83603996+ChristelPeerisR@users.noreply.github.com>
2024-03-09 13:18:02 +0530

    Update validate.js

--- validate.js
+++ validate.js
@@ -56,7 +56,7 @@ function errorFunc(re, msg) {
     if (re !== email) {
         re.value = '';
     } else {
-        re.style.color = "hsl(0, 100%, 74%)";
+        re.style.color = "rgba(55, 204, 138, 255)";
     }
 }
 
